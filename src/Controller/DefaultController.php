72853a22882484539a1551ccc6057590b910e132
quentin <qloustau@gmail.com>
2018-11-12 14:59:06 +0100

    .idea ne doit pas etre mis sur git

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -12,6 +12,15 @@ class DefaultController extends AbstractController
      */
     public function index()
     {
+        $faker = \Faker\Factory::create();
+
+// generate data by accessing properties
+        echo $faker->name;
+        // 'Lucy Cechtelar';
+        echo $faker->address;
+        // "426 Jordy Lodge
+        // Cartwrightshire, SC 88120-6700"
+        echo $faker->text;
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
         ]);
